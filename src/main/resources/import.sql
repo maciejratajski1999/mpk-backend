43b4c4fae53dfc0c7641b411b81e63004904f1ce
Maciej Ratajski <maciejratajski1999@gmail.com>
2024-05-01 17:18:35 +0200

    hibernate database/nowe importy

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,12 +1,11 @@
-INSERT INTO Vehicle(id, vehicle_id)
-VALUES (1, 2024);
-INSERT INTO Vehicle(id, vehicle_id)
-VALUES (2, 2025);
-INSERT INTO Route(route_id, route_type, valid_from)
-VALUES (101, 1, '2024-01-01');
-INSERT INTO Route(route_id, route_type, valid_from)
-VALUES (102, 2, '2024-02-01');
-INSERT INTO Trip(trip_id, route_id, trip_headsign, direction_id, shape_id, variant_id)
-VALUES ( 201202, 101, '1 POŚWIĘTNE', 1, 301, 401);
-INSERT INTO Trip(trip_id, route_id, trip_headsign, direction_id, shape_id, variant_id)
-VALUES (202203, 102, '16 KARŁOWICE', 2, 302, 402);
+-- Przykłady dla encji Route
+INSERT INTO Route(route_id, route_type, valid_from) VALUES ('R1', 1, '2024-04-27');
+INSERT INTO Route(route_id, route_type, valid_from) VALUES ('R2', 2, '2024-04-28');
+
+-- Przykłady dla encji Trip
+INSERT INTO Trip(trip_id, route_id, trip_headsign, direction_id, shape_id, variant_id) VALUES (1, 'R1', 'HeadSign1', 1, 100, 10);
+INSERT INTO Trip(trip_id, route_id, trip_headsign, direction_id, shape_id, variant_id) VALUES (2, 'R2', 'HeadSign2', 2, 200, 20);
+
+-- Przykłady dla encji Vehicle
+INSERT INTO Vehicle(id, vehicle_id) VALUES (1, 1001);
+INSERT INTO Vehicle(id, vehicle_id) VALUES (2, 1002);
