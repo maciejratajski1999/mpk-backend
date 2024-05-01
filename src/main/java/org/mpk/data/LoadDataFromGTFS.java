85165fac57d6a747148ff67f8068dffd18d06ff1
Maciej Ratajski <maciejratajski1999@gmail.com>
2024-05-01 08:29:59 +0200

    parse'owanie txt do HashMapy oraz test

--- src/main/java/org/mpk/data/LoadDataFromGTFS.java
+++ src/main/java/org/mpk/data/LoadDataFromGTFS.java
@@ -1,2 +1,24 @@
-package org.mpk.data;public class ParseTxtFileGTFS {
+package org.mpk.data;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoadDataFromGTFS {
+
+    public Map<String, String> parseTxtFile(Path path) {
+        Map<String, String> fileData = new HashMap<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(path.toFile()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] fields = line.split(",");
+                fileData.put(fields[0], fields[1]);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return fileData;
+    }
 }
--- src/test/java/org/mpk/ParseDataFromTxtTest.java
+++ src/test/java/org/mpk/ParseDataFromTxtTest.java
@@ -2,40 +2,29 @@ package org.mpk;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mpk.data.LoadDataFromGTFS;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
-public class LoadDataFromGTFS {
+public class ParseDataFromTxtTest {
 
     @Test
     public void testFileProcessing() throws Exception {
+        LoadDataFromGTFS processor = new LoadDataFromGTFS();
         Map<String, Map<String, String>> data = new HashMap<>();
         Path dir = Paths.get("src/test/data");
         Files.walk(dir)
                 .filter(Files::isRegularFile)
                 .filter(path -> path.toString().endsWith(".txt"))
                 .forEach(path -> {
-                    Map<String, String> fileData = new HashMap<>();
-                    try (BufferedReader reader = new BufferedReader(new FileReader(path.toFile()))) {
-                        String line;
-                        while ((line = reader.readLine()) != null) {
-                            String[] fields = line.split(",");
-                            fileData.put(fields[0], fields[1]);
-                        }
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
+                    Map<String, String> fileData = processor.parseTxtFile(path);
                     data.put(path.getFileName().toString(), fileData);
                 });
 
-        // Tutaj możesz dodać asercje sprawdzające, czy dane zostały poprawnie przetworzone
-        // Na przykład:
         Assertions.assertTrue(data.containsKey("routes.txt"));
     }
 }
