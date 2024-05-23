43f123b9bdb5e57eaad41236d0ba863bac0e443d
Maciej Ratajski <maciejratajski1999@gmail.com>
2024-05-23 19:55:53 +0200

    zwracaj najnowesze pozycje pojazdów, na razie wszystkich

--- src/main/java/org/mpk/resource/VehiclePositionResource.java
+++ src/main/java/org/mpk/resource/VehiclePositionResource.java
@@ -1,15 +1,23 @@
 package org.mpk.resource;
 
 import io.quarkus.hibernate.reactive.panache.Panache;
+import io.quarkus.hibernate.reactive.panache.PanacheEntityBase;
+import io.quarkus.logging.Log;
+import io.quarkus.panache.common.Sort;
+import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Response;
+import org.mpk.entity.Vehicle;
 import org.mpk.entity.VehiclePosition;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static jakarta.ws.rs.core.Response.Status.CREATED;
+
 @Path("/vehiclepositions")
 @ApplicationScoped
 @Produces("application/json")
@@ -27,19 +35,34 @@ public class VehiclePositionResource {
         return VehiclePosition.findById(posId);
     }
 
-//    @POST
-//    public Uni<Response> create(VehiclePosition vehiclePosition) {
-//        if (vehiclePosition == null){
-//            throw new WebApplicationException("VehiclePosition is null", 422);
-//        }
-//        return Panache.withTransaction(vehiclePosition::persist)
-//                .replaceWith(Response.ok(vehiclePosition).status(CREATED)::build);
-//    }
-
     @POST
     public Uni<Response> create(String vehiclePositionEntry) {
         return VehiclePosition.fromString(vehiclePositionEntry)
                 .onItem().transformToUni(vehiclePosition -> Panache.withTransaction(vehiclePosition::persist))
                 .replaceWith(Response.ok().status(CREATED)::build);
     }
+
+    @GET
+    @Path("/latest")
+    public Uni<List<VehiclePosition>> getLatestPositionsForRoutes(@QueryParam("routeIds") String routeIdsString) {
+        List<String> routeIds = Arrays.asList(routeIdsString.split(","));
+        if (routeIds.isEmpty()) {
+            return VehiclePosition.findById(0)
+                    .onItem().transform(testPosition -> List.of((VehiclePosition) testPosition));
+        }
+        routeIds.forEach(routeId -> Log.info(routeId.toString()));
+
+        // Zwraca wszystkie VehiclePosition, które mają vehicle.trip.route.routeId w liście routeIds
+//        return Vehicle.listAll()
+//                .onItem().transformToMulti(vehicles -> Multi.createFrom().iterable(vehicles))
+//                .onItem().castTo(Vehicle.class)
+//                .filter(vehicle -> routeIds.contains(vehicle.trip.route.routeId))
+//                .onItem().transformToUniAndConcatenate(vehicle ->
+//                    VehiclePosition.find("vehicle.vehicleID = ?1 ORDER BY timestamp DESC", (VehiclePosition) vehicle.vehicleID)
+//                    .firstResult())
+//                .collect().asList();
+        return  VehiclePosition.findAll(Sort.by("timestamp").descending()).list();
+    }
+
+
 }
